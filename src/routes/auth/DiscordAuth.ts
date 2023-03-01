2fd00713189e1ddbcd45d198df902e4facc87296
cjs8487 <cjschaefer1999@gmail.com>
2023-02-28 19:18:02 -0600

    regenerate session to prevent session fixation

--- src/routes/auth/DiscordAuth.ts
+++ src/routes/auth/DiscordAuth.ts
@@ -1,20 +1,34 @@
-import { Router } from 'express';
+import { isAxiosError } from 'axios';
+import { NextFunction, Router } from 'express';
+import { logError } from '../../Logger';
 import { exchangeCode } from '../../core/auth/DiscordTokens';
 
 const discordAuth = Router();
 
-discordAuth.post('/authorized', async (req, res) => {
+discordAuth.post('/authorized', async (req, res, next: NextFunction) => {
     const { code } = req.body;
     try {
         const token = await exchangeCode(code);
-        req.session.loggedIn = true;
-        req.session.token = token.accessToken;
-        req.session.refresh = token.refreshToken;
+        req.session.regenerate((err) => {
+            if (err) next(err);
+
+            req.session.loggedIn = true;
+            req.session.token = token.accessToken;
+            req.session.refresh = token.refreshToken;
+
+            req.session.save((saveErr) => {
+                if (saveErr) next(saveErr);
+                res.status(200).send();
+            });
+        });
     } catch (e) {
+        if (isAxiosError(e)) {
+            logError(e.message);
+        } else {
+            logError(`An unknown error ocurred while handling a request - ${e}`);
+        }
         res.status(500).send();
-        return;
     }
-    res.status(200).send();
 });
 
 export default discordAuth;
