71702f376a1b31f5a9964783f96190bdb2154c3f
cjs8487 <cjschaefer1999@gmail.com>
2023-08-12 15:10:21 -0500

    fix db migration quirk in dev environemnts

--- src/System.ts
+++ src/System.ts
@@ -12,7 +12,7 @@ import { DiscordDataManager } from './database/DiscordDataManager';
 // this setup sequence makes several assumptions
 //  - the existence of the directory src/database/scripts
 //  - the directory contains ONLY files named v1...vx, where v1 is the base script
-//          and the following scripts modify existing properties
+//          and all subsequent scripts will either create new objects or modify existing ones
 //  - the files are named sequentially
 //  - no files touch the user_version pragma; it is maintained as part of this sequence
 //
@@ -21,7 +21,6 @@ import { DiscordDataManager } from './database/DiscordDataManager';
 // file, and the sequence will set the stored version back by one. It is assumed that the backup
 // will always be the result of the second to last migration script being run. This allows the
 // database to kept in sync during the dev cycle without making manual changes to it
-logInfo('starting database migration');
 if (testing) {
     logInfo('copying database from backup');
     copyFileSync('db backup.db', 'database.db');
@@ -39,45 +38,33 @@ const dbScriptDir = 'src/database/scripts';
 const migrationFileNames = readdirSync(dbScriptDir).sort((a, b) =>
     a.localeCompare(b, undefined, { numeric: true, sensitivity: 'base' }),
 );
-migrationFileNames.forEach((migrationFileName, index) => {
-    if (dbVersion > index) return;
-    logInfo(`migrating with script ${migrationFileName}`);
-    const script = readFileSync(`${dbScriptDir}/${migrationFileName}`, 'utf-8');
-    db.exec(script);
-});
-const newVersion = migrationFileNames.length;
-db.pragma(`user_version=${newVersion}`);
-logInfo(`database migrated to v${newVersion}`);
+if (migrationFileNames.length > dbVersion) {
+    logInfo('starting database migration');
+    // we'll always back up the database before runnning any migrations
+    // if we're in a dev environment, this is essentially a no-op, since we just copied the backup to the real database
+    // if we're in production, it'll make a backup of the (theoretically) last known good database (there is a edge
+    //      case if migrations are re-ran after a failure mid-migration, but production instances should be maintaining
+    //      additional backups anyway, this backup is intended for quick recovery if an update fails)
+    logInfo('backing up database file');
+    copyFileSync('database.db', 'db backup.db');
+    migrationFileNames.forEach((migrationFileName, index) => {
+        if (dbVersion > index) return;
+        logInfo(`migrating with script ${migrationFileName}`);
+        const script = readFileSync(
+            `${dbScriptDir}/${migrationFileName}`,
+            'utf-8',
+        );
+        db.exec(script);
+    });
+    const newVersion = migrationFileNames.length;
+    db.pragma(`user_version=${newVersion}`);
+    logInfo(`database migrated to v${newVersion}`);
 
-if (testing) {
-    logInfo('un-migrating database by one version');
-    db.pragma(`user_version=${newVersion - 1}`);
+    if (testing) {
+        logInfo('un-migrating database by one version');
+        db.pragma(`user_version=${newVersion - 1}`);
+    }
 }
-
-// db.exec('pragma foreign_keys=off');
-// const dbScriptDir = 'src/database/scripts';
-// const tables = db
-//     .prepare("select name from sqlite_master where type='table'")
-//     .all()
-//     .filter((table: { name: string }) => !table.name.startsWith('sqlite_'))
-//     .map((table: { name: string }) => table.name);
-// tables.forEach((table) => {
-//     logInfo(`Backing up ${table}`);
-//     db.exec(`drop table if exists ${table}_temp`);
-//     db.exec(`create table ${table}_temp as select * from ${table}`);
-//     db.exec(`drop table ${table}`);
-// });
-
-// logInfo('Running database initialization');
-// const setupScript = readFileSync(`${dbScriptDir}/dbsetup.sql`, 'utf-8');
-// db.exec(setupScript);
-
-// tables.forEach((table) => {
-//     logInfo(`Restoring ${table} from backup and cleaning up`);
-//     db.exec(`insert into ${table} select * from ${table}_temp`);
-//     db.exec(`drop table ${table}_temp`);
-// });
-// db.exec('pragma foreign_keys=on');
 
 export const sessionsDb: DB = new Database('sessions.db');
 
